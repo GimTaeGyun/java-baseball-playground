cf391d73a25bad1b5201306b32e26b25f7bc816d
gwangchulddddgimtdkfㅇㅇㄹ <xorbsdut@naver.com>
2022-05-08 22:43:15 +0900

    feaf : 숫자 야구 게임 0단계 완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -10,4 +11,32 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void request1() {
+        //요구사항 1
+        String[] actual1 = "1,2".split(",");
+        assertThat(actual1).contains("1", "2");
+
+        String[] actual2 = "1".split(",");
+        assertThat(actual2).containsExactly("1");
+    }
+
+    @Test
+    void request2() {
+        //요구사항 2
+        String actual1 = "(1,2)".substring(1,4);
+        assertThat(actual1).isEqualTo("1,2");
+    }
+
+    @DisplayName("charAt 테스트")
+    void request3() {
+        //요구사항 3
+//        String actual3 = "abc".charAt();
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+            .isThrownBy(() -> {
+
+        }).withMessageMatching("Index: \\d+, Size: \\d+");
+    }
+
 }
